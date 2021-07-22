c442b22fe5bb57ad1a782d3cc80cf981126b6f38
Isaac Lewis <isaac.b.lewis@gmail.com>
2021-07-22 17:46:40 -0400

    1) Remove outdated documentation from the code, and 2) remove 'toNumerals' from the API section.

--- num_dec.py
+++ num_dec.py
@@ -1,111 +1,6 @@
 import re
 from decimal import *
 
-#+MOTIVATION----------------------------------------------------------
-#
-# This library may be used to translate numerals to decimals and vice
-# versa. It is intended to be used with the general dictation produced
-# by an SR engine such as the one in Dragon. Libraries similar to this
-# one already exist; but all of the ones I have found were either
-# closed source or lacked the features that I was looking for.
-#
-#+TESTING & QUALITY --------------------------------------------------
-#
-# The API methods were tested for the first 1 billion integers with
-# the chunk size ranging between -4 and 1. An example should make
-# the meaning of 'chunk size' clear.
-#
-# EXAMPLE: Convert the decimal 123456 to numerals using (a) a chunk
-# size of 4 and (b) a chunk size of -4.
-#
-# Solution:
-# (a) Since the chunk size is positive, split the decimal into chunks
-# from left to right.
-#
-# 123456 -> 1234,56-> one thousand two hundred thirty four fifty six
-#
-# (b) Since the chunk size is negative, split the decimal into chunks
-# from right to left.
-#
-# 123456 -> 12,3456-> twelve three thousand four hundred fifty six
-#
-# Why is chunking important? Because it makes manual data entry much
-# easier.  It is easier to say 'ten ten ten' than 'one million ten
-# thousand ten' to enter the number 101010.
-#
-#+INVERTIBILITY OF CHUNKING-------------------------------------------
-#
-# Converting decimals to numerals by chunking from left to right is
-# not always invertible, since 970053->9700,53->ninety seven hundred
-# fifty three->9753. Curiously chunking from right to left works:
-# 970053->97,0053->ninety seven zero zero fifty three->970053. In
-# general,
-#
-# (1) Converting decimals to numerals by chunking from right to left
-#     is invertible.
-#
-# The result is not particularly convenient since the reading order in
-# English is from left to right. However, at least one useful
-# consequence may gleaned from (1):
-#
-# (2) Converting decimals to numerals by chunking is invertible if all
-#     chunks are of the same size.
-#
-# Thus chunking digit-by-digit will always work.
-#
-#+PUBLIC API----------------------------------------------------------
-#
-# The API consists of two functions:
-#
-# 1) toDecimal(str)
-#
-# which takes a single argument: <str> -- a string of space separated
-#                                         English words possibly
-#                                         including punctuation
-#
-# 2) toNumerals(str, int)
-#
-# which takes two arguments: <str> -- a string of space separated
-#                                       English words possibly
-#                                       including punctuation
-#
-#                            <int> -- an integer indicating the
-#                                     chunk size
-#
-#+KEY FEATURES of 'toDecimal'-----------------------------------------
-#
-# 1) Supports chunking:
-#
-#    toDecimal("four five nine two") = 4592
-#
-# 2) Does not alter non-numerals:
-#
-#    toDecimal("I'll have thirteen eggs for breakfast.") =
-#    "I'll have 13 eggs for breakfast."
-#
-# 3) Can handle long dictations:
-#
-#    toDecimal("one two three four five six seven eight nine ten") =
-#    12345678910
-#
-#+KEY FEATURES of 'toNumerals'----------------------------------------
-#
-# 1) Supports chunking:
-#
-#    toNumerals("4592",2) = "forty five ninety two"
-#
-# 2) Does not alter non-decimals:
-#
-#    toNumerals("The number A8 in hexadecimal is 168 in decimal.",0) =
-#    "The number A8 in hexadecimal is one hundred sixty eight in
-#    decimal."
-#
-# 3) Can handle large decimals:
-#
-#    toNumerals("12345678910",1) =
-#    "one two three four five six seven eight nine ten"
-#
-
 #+UTILITY FUNCTIONS---------------------------------------------------
 
 def parts_helper(s, ps, res):
@@ -386,23 +281,6 @@ def toNumeralsHelper(s, g):
 
     return l3
 
-#PUBLIC API-----------------------------------------------------------
-
-def toDecimal(s):
-    """Replaces any sequences of numerals in the string <s> with their
-    corresponding decimals.
-
-    Examples:
-    toDecimal("one thousand eight hundred fifty eight") = "1858"
-    toDecimal("eighteen hundred fifty eight") = "1858"
-    toDecimal("eighteen fifty eight") = "1858"
-    toDecimal("one eight five eight") = "1858"
-    """
-    numeral = "(?:" + "|".join(numerals_desc) + ")"
-    numeral_seq = "(?: |^)("+numeral+"(?: " + numeral + ")*)(?: |$)"
-
-    return re.sub(numeral_seq, toDecimalHelper, s)
-
 def toNumerals(s, g):
     """Return the string that results from replacing all decimals in
     the string <s> with their corresponding numerals using chunks of
@@ -427,6 +305,23 @@ def toNumerals(s, g):
     toNumerals("1858","-4*") = "eighteen hundred fifty eight"
     """
     return re.sub("( |^)([0-9]+)( |$)", lambda m: m.group(1)+" ".join(toNumeralsHelper(m.group(2), g))+m.group(3), s)
+
+#PUBLIC API-----------------------------------------------------------
+
+def toDecimal(s):
+    """Replaces any sequences of numerals in the string <s> with their
+    corresponding decimals.
+
+    Examples:
+    toDecimal("one thousand eight hundred fifty eight") = "1858"
+    toDecimal("eighteen hundred fifty eight") = "1858"
+    toDecimal("eighteen fifty eight") = "1858"
+    toDecimal("one eight five eight") = "1858"
+    """
+    numeral = "(?:" + "|".join(numerals_desc) + ")"
+    numeral_seq = "(?: |^)("+numeral+"(?: " + numeral + ")*)(?: |$)"
+
+    return re.sub(numeral_seq, toDecimalHelper, s)
 
 #TESTING--------------------------------------------------------------
 
